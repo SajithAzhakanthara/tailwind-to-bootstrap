636abdfba827f49fb918ebc2cfe053697e3bbff6
SajithAzhakanthara <52636121+SajithAzhakanthara@users.noreply.github.com>
2025-01-20 17:39:26 +0530

    completed

---
+++ tailwind-to-bootstrap/src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+      <div className="d-flex align-items-center justify-content-center h-100vh">
+        <h1 className="m-0">Tailwind UI to Bootstrap UI with Next Js</h1>
+      </div>
+  );
+}
